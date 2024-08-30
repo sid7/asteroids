2edb45cd0b8d09169102d758407bae59ed0542a5
Suyash Bhutani <sid7@users.noreply.github.com>
2024-08-30 20:20:36 +0530

    complete required steps of boot.dev project Build Asteroids guided project

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
+import sys
+
 import pygame
-from constants import *
+
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)  # player.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)  # player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Asteroids
+
+A simple [Asteroids](<https://en.wikipedia.org/wiki/Asteroids_(video_game)>) game build using pygame, built for [boot.dev](boot.dev) Guided Project [Build Asteroids](https://www.boot.dev/courses/build-asteroids)
